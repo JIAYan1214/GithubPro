bcc265923392ab135714d87d20cb63a35e12edaa
JIAYan1214 <jyy_1214@163.com>
2019-10-30 16:05:59 +0800

    完善首页，

--- pages-test/_app.js
+++ pages-test/_app.js
@@ -6,15 +6,17 @@
  * 3.使用componentDidCatch自定义处理错误
  * 4.注入额外数据到页面里 (如 GraphQL 查询)
  */
-import App ,{Container} from 'next/app';
+import App,{Container} from 'next/app';
 import React from 'react';
 import  {Provider} from 'react-redux';
-import Layout from '../components/Layout';
-import WithRedux from '../lib/with-redux';
+import Layout from '../components/learn-base/Layout';
+import MyContext from '../lib/MyContext';
 
+// import store from '../store/store';
+import WithRedux from '../lib/with-redux';
 import 'antd/dist/antd.css';
 
-class MyApp extends App {
+class MyApp extends App{
     //每次页面切换都执行此方法
     static async getInitialProps(ctx){
         const {Component} = ctx;
@@ -25,17 +27,20 @@ class MyApp extends App {
         return {pageProps};
     }
 
-    render() {
-        const {Component, pageProps,reduxStore} = this.props;
+    render(){
+        const { Component,pageProps,reduxStore } = this.props;
         return (
             <Container>
                 <Layout>
                     <Provider store={reduxStore}>
-                        <Component {...pageProps} />
+                    <MyContext.Provider value="test">
+                        <Component {...pageProps}/>
+                    </MyContext.Provider>
                     </Provider>
                 </Layout>
             </Container>
         )
     }
 }
+
 export default WithRedux(MyApp);
